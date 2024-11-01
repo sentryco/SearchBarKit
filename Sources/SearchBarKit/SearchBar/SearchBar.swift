37c072b11a2ad740949e3f6ef724f8fab144d51b
eonist <30n1st@gmail.com>
2024-11-01 10:59:47 +0100

    Update SearchBar.swift

--- Sources/SearchBarKit/SearchBar/SearchBar.swift
+++ Sources/SearchBarKit/SearchBar/SearchBar.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 /**
  * Contains the `SearchField` and related UI elements
- * - Description: The `SearchBar` struct defines a user interface component for text-based search functionality within an app. It includes a text field for user input, a placeholder to indicate the intended use, and callbacks for text changes and focus events. The component is designed to integrate seamlessly with SwiftUI views and provides a customizable and interactive search experience.
+ * - Description: The `SearchBar` struct defines a user interface component
+ *                for text-based search functionality within an app. It
+ *                includes a text field for user input, a placeholder to
+ *                indicate the intended use, and callbacks for text changes
+ *                and focus events. The component is designed to integrate
+ *                seamlessly with SwiftUI views and provides a customizable
+ *                and interactive search experience.
  * - Fixme: ⚠️️⚠️️ the `onTextChange` should probably just be a 👉 binding 👈, so maybe remove that? fix it later when transfereing code and comments from legacy etc
  * - Fixme: ⚠️️⚠️️ make `onFocus` a binding instead? 👈 fix it later when transfering code and comments from legacy etc
  * - Fixme: ⚠️️ Possibly customize `NSSearchField` in the future etc (less custom code etc, do it when migrating to SwiftUI / Catalyst)
@@ -14,7 +20,9 @@ public struct SearchBar: View {
    internal let placeholderText: String
    /**
     * Callback executed when the search bar gains focus.
-    * - Description: The callback function that is invoked when the search bar either gains or loses focus, allowing the parent view to react to focus changes.
+    * - Description: The callback function that is invoked when the 
+    *                search bar either gains or loses focus, allowing 
+    *                the parent view to react to focus changes.
     * - Fixme: ⚠️️ make this a binding or remove? or use textFieldIsFocused?
     * - Parameters:
     *   - isFocused: A boolean indicating whether the search bar is now focused.
@@ -22,7 +30,10 @@ public struct SearchBar: View {
    internal let onFocus: OnFocus // = defaultOnFocus
    /**
     * Callback executed when the text in the search bar changes.
-    * - Description: The callback function that is invoked when the user changes the text in the search bar. It allows the parent view to react and handle the updated search text in real-time.
+    * - Description: The callback function that is invoked when the user 
+    *                changes the text in the search bar. It allows the 
+    *                parent view to react and handle the updated search 
+    *                text in real-time.
     * - Parameters:
     *   - text: The new text in the search bar.
     */
@@ -35,7 +46,10 @@ public struct SearchBar: View {
    @State internal var searchText: String = ""
    /**
     * Tracks the focus state of the text field in the search bar.
-    * - Description: This property tracks whether the text field in the search bar is currently focused or not. It is used to manage the keyboard visibility and the state of the search bar.
+    * - Description: This property tracks whether the text field in the 
+    *                search bar is currently focused or not. It is used 
+    *                to manage the keyboard visibility and the state of 
+    *                the search bar.
     * - Note: Ref: https://www.hackingwithswift.com/quick-start/swiftui/how-to-dismiss-the-keyboard-for-a-textfield
     * - Remark: Hides the clear button if not in "search-mode", sets the background style, and forwards the callback
     * - Remark: This highlights the text in the search bar and sets focus to the text field.
@@ -53,7 +67,12 @@ public struct SearchBar: View {
    internal static var searchBarTheme: SearchBarTheme = SearchBarTheme.defaultTheme
    /**
     * Initializes a SearchBar with the specified parameters.
-    * - Description: Initializes a `SearchBar` with a specific placeholder text, focus callback, and text change callback. The placeholder text is displayed when the search bar is empty. The focus callback is invoked when the search bar gains or loses focus, and the text change callback is invoked when the text in the search bar changes.
+    * - Description: Initializes a `SearchBar` with a specific placeholder 
+    *                text, focus callback, and text change callback. The 
+    *                placeholder text is displayed when the search bar is 
+    *                empty. The focus callback is invoked when the search 
+    *                bar gains or loses focus, and the text change callback 
+    *                is invoked when the text in the search bar changes.
     * - Fixme: ⚠️️ Move the placeholderText into const Title.searchText
     * - Fixme: ⚠️️ Localize the placeholder text, see legacy
     * - Parameters:
